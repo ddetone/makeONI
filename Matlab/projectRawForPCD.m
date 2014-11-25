3ecae1d64956725227f950da79bbeafee63e0b7f
ddetone <daniel.detone@gmail.com>
2014-11-25 16:02:29 -0500

    parallelize

--- Matlab/projectRawForPCD.m
+++ Matlab/projectRawForPCD.m
@@ -1,22 +1,25 @@
-data_path = '../Data/raw/';
-dataset = 'basement_0001a';
-
-sceneDir = [data_path dataset];
-frameList = get_synched_frames(sceneDir);
+% data_path = '../Data/raw/';
+% dataset = 'basement_0001a';
+% 
+% sceneDir = [data_path dataset];
+% frameList = get_synched_frames(sceneDir);
+% 
+% camera_params;
+% kc_d = [k1_d, k2_d, p1_d, p2_d, k3_d];
+% fc_d = [fx_d,fy_d];
+% cc_d = [cx_d,cy_d]; 
+% 
+% fc_rgb = [fx_rgb,fy_rgb];
+% cc_rgb = [cx_rgb,cy_rgb]; 
+% kc_rgb = [k1_rgb,k2_rgb,p1_rgb,p2_rgb,k3_rgb];
 
 % Displays each pair of synchronized RGB and Depth frames.
-for ii = 1 : 1 : numel(frameList)
+matlabpool open
+parfor ii = 1 : 1 : numel(frameList)
   imgRgb = imread([sceneDir '/' frameList(ii).rawRgbFilename]);
   imgDepth = swapbytes(imread([sceneDir '/' frameList(ii).rawDepthFilename]));
 
-  camera_params;
-  kc_d = [k1_d, k2_d, p1_d, p2_d, k3_d];
-  fc_d = [fx_d,fy_d];
-  cc_d = [cx_d,cy_d]; 
 
-  fc_rgb = [fx_rgb,fy_rgb];
-  cc_rgb = [cx_rgb,cy_rgb]; 
-  kc_rgb = [k1_rgb,k2_rgb,p1_rgb,p2_rgb,k3_rgb];
   noiseMask = 255 * double(imgDepth == max(imgDepth(:)));
 
   % Undistort the noise mask.
@@ -27,7 +30,8 @@ for ii = 1 : 1 : numel(frameList)
 
   % Fix issues introduced by distortion.
   imgDepth(imgDepth < 600) = 2047;
-  imgDepth(noiseMask) = 2047;
+  imgDepth(noiseMask) = 2047;We did a quick mashup of our API and this to create a basic (but functional) mobile PoS using the phone camera as a scanner.
+
   depth2 = depth_rel2depth_abs(imgDepth);
   points3d = depth_plane2depth_world(depth2);
   
@@ -37,3 +41,4 @@ for ii = 1 : 1 : numel(frameList)
   savepcd(fname, points3d);
   
 end
+matlabpool close
