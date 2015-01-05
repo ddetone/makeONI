76d856eff2611cb7fab68fdfaecc314241e66da5
ddetone <daniel.detone@gmail.com>
2015-01-05 13:47:50 -0500

    use some parallelization in matlab

--- Matlab/projectRawForPCD.m
+++ Matlab/projectRawForPCD.m
@@ -1,21 +1,24 @@
-% data_path = '../Data/raw/';
-% dataset = 'basement_0001a';
-% 
-% sceneDir = [data_path dataset];
-% frameList = get_synched_frames(sceneDir);
-% 
-% camera_params;
-% kc_d = [k1_d, k2_d, p1_d, p2_d, k3_d];
-% fc_d = [fx_d,fy_d];
-% cc_d = [cx_d,cy_d]; 
-% 
-% fc_rgb = [fx_rgb,fy_rgb];
-% cc_rgb = [cx_rgb,cy_rgb]; 
-% kc_rgb = [k1_rgb,k2_rgb,p1_rgb,p2_rgb,k3_rgb];
+data_path = '../Data/raw/';
+dataset = 'study_0002';
+
+
+load 'nyu_depth_v2_labeled';
+
+sceneDir = [data_path dataset];
+frameList = get_synched_frames(sceneDir);
+
+camera_params;
+kc_d = [k1_d, k2_d, p1_d, p2_d, k3_d];
+fc_d = [fx_d,fy_d];
+cc_d = [cx_d,cy_d]; 
+
+fc_rgb = [fx_rgb,fy_rgb];
+cc_rgb = [cx_rgb,cy_rgb]; 
+kc_rgb = [k1_rgb,k2_rgb,p1_rgb,p2_rgb,k3_rgb];
 
 % Displays each pair of synchronized RGB and Depth frames.
-matlabpool open
-parfor ii = 1 : 1 : numel(frameList)
+% matlabpool open
+for ii = 1 : 1 : 50
   imgRgb = imread([sceneDir '/' frameList(ii).rawRgbFilename]);
   imgDepth = swapbytes(imread([sceneDir '/' frameList(ii).rawDepthFilename]));
 
@@ -30,7 +33,8 @@ parfor ii = 1 : 1 : numel(frameList)
 
   % Fix issues introduced by distortion.
   imgDepth(imgDepth < 600) = 2047;
-  imgDepth(noiseMask) = 2047;We did a quick mashup of our API and this to create a basic (but functional) mobile PoS using the phone camera as a scanner.
+  imgDepth(noiseMask) = 2047;
+  %We did a quick mashup of our API and this to create a basic (but functional) mobile PoS using the phone camera as a scanner.
 
   depth2 = depth_rel2depth_abs(imgDepth);
   points3d = depth_plane2depth_world(depth2);
@@ -41,4 +45,4 @@ parfor ii = 1 : 1 : numel(frameList)
   savepcd(fname, points3d);
   
 end
-matlabpool close
+% matlabpool close
